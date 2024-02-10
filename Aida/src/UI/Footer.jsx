b83b57dc409c3d1736dfd9d4ac6aec3cb626760e
Rana <ranamohamad215@gmail.com>
2024-02-10 23:35:16 +0200

    Initial Login page structure.

---
+++ Aida/src/UI/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer className="bg-teal-[rgb(3,188,191)]">
+      <hr />
+      {/*Roboto font*/}
+      <p>&copy; AIDA all rights reserved</p>
+    </footer>
+  );
+}
+
+export default Footer;
