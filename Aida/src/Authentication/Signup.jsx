7b8c7e41b7b1d8927597f8b2345cf6a1243f0628
Rana <ranamohamad215@gmail.com>
2024-02-25 14:05:47 +0200

    Updated Costumer Signup page

--- Aida/src/Authentication/Signup.jsx
+++ Aida/src/Authentication/Signup.jsx
@@ -1,4 +1,27 @@
 import LogoTealText from "../assets/logo/LogoTealText.svg";
+// import categories from "../UI/SignupCategories";
+const inputStyle = "border-solid border-2 border-gray px-2 py-1 mx-2 mt-2 mb-4";
+const labelStyle = "font-semibold mx-2  ";
+import sport from "../assets/Authentication/Categories/sport1.jpeg";
+import petSupplies from "../assets/Authentication/Categories/petsupplies.jpeg";
+import officeSupplies from "../assets/Authentication/Categories/office supplies.jpeg";
+import fashion from "../assets/Authentication/Categories/fashion2.jpeg";
+import healthCare from "../assets/Authentication/Categories/healthcare1.jpeg";
+import electronics from "../assets/Authentication/Categories/Electronics2.jpeg";
+import books from "../assets/Authentication/Categories/books2.jpeg";
+import homeSupplies from "../assets/Authentication/Categories/home supplies.jpeg";
+
+const categories = [
+  { name: "Fashion", link: fashion },
+  { name: "Books", link: books },
+  { name: "Electronic devices", link: electronics },
+  { name: "Health & wellness", link: healthCare },
+  { name: "Office Supplies", link: officeSupplies },
+  { name: "Pet Supplies", link: petSupplies },
+  { name: "Home supplies", link: homeSupplies },
+  { name: "Sporting goods", link: sport },
+];
+
 function Signup() {
   return (
     <>
@@ -7,7 +30,7 @@ function Signup() {
           <img
             src="/src/assets/Authentication/SidebarSignup.jpeg"
             alt="Side Bar"
-            className="w-full object-fill"
+            className="w-full object-cover"
           />
         </div>
         <div className=" w-9/12 px-14">
@@ -16,119 +39,101 @@ function Signup() {
             alt="Logo"
             className="w-[410px] h-[211px] ml-auto mr-auto"
           />
-          <form>
-            <div className="flex">
-              <div>
+          <form className="mb-3">
+            <div className="flex justify-around  w-2/3 relative h-3/4 mx-auto font-sans">
+              <div className="w-1/2 bg-gray">
                 <div>
-                  <label>First Name</label> <br />
-                  <input type="text " placeholder="Ahmad" />
+                  <label className={labelStyle}>First Name</label> <br />
+                  <input
+                    type="text "
+                    placeholder="Ahmad"
+                    className={inputStyle}
+                  />
                 </div>
                 <div>
-                  <label>Email</label>
+                  <label className={labelStyle}>Email</label>
                   <br />
-                  <input type="email" placeholder="example@example.com" />
+                  <input
+                    type="email"
+                    placeholder="example@example.com"
+                    className={inputStyle}
+                  />
                 </div>
                 <div>
-                  <label>Adress</label>
+                  <label className={labelStyle}>Adress</label>
                   <br />
                   <input
                     type="text"
                     placeholder="Qesm ElMoqatam, Cairo, Egypt"
+                    className={inputStyle}
                   />
-                  <span>This will be the delivery address</span>
+                  <p className="font-thin text-xs ">
+                    This will be the delivery address
+                  </p>
+                </div>
+                <div className="mt-4">
+                  <label className={labelStyle}>Date of birth</label>
+                  <br />
+                  <input type="date" className={inputStyle} />
                 </div>
                 <div>
-                  <label>Date of birth</label>
+                  <label className={labelStyle}>Account</label>
                   <br />
-                  <input type="date" />
-                </div>
-                <div>
-                  <label>Account</label>
-                  <br />
-                  <input type="radio" value="Shopping" />{" "}
-                  <label>Shopping</label>
+                  <input type="radio" value="Shopping" className={inputStyle} />
+                  <label className={labelStyle}>Shopping</label>
                   <input type="radio" value="Selling" /> <label>Selling</label>
                   <p className="text-FlamingoPink">
                     This cannot be changed later
                   </p>
                 </div>
               </div>
-              <div>
+              <div className="w-1/2 bg-IceBlue">
                 <div>
-                  <label>Second Name</label>
+                  <label className={labelStyle}>Second Name</label>
                   <br />
-                  <input type="text" placeholder="Taha" />
+                  <input
+                    type="text"
+                    placeholder="Taha"
+                    className={inputStyle}
+                  />
                 </div>
                 <div>
-                  <label>Password</label>
+                  <label className={labelStyle}>Password</label>
                   <br />
-                  <input type="password" />
+                  <input type="password" className={inputStyle} />
                 </div>
                 <div>
-                  <label>Phone</label>
+                  <label className={labelStyle}>Phone</label>
                   <br />
-                  <input type="tel" placeholder="+201232453123" />
+                  <input
+                    type="tel"
+                    placeholder="+201232453123"
+                    className={inputStyle}
+                  />
                 </div>
-                <div>
-                  <label>Gender</label>
+                <div className="mt-8">
+                  <label className={labelStyle}>Gender</label>
                   <br />
                   <input type="radio" value="Male" /> <label>Male</label>
-                  <input type="radio" value="Female" /> <label>Female</label>
+                  <input type="radio" value="Female" />{" "}
+                  <label className={labelStyle}>Female</label>
                 </div>
               </div>
             </div>
-            <div>
-              <p className="bold">
-                Select categories that you might be interested in
-              </p>
-              <table>
-                <tbody>
-                  <tr>
-                    <td>
-                      <input
-                        type="checkbox"
-                        id="image1"
-                        name="images[]"
-                        value="image1"
-                      />
-                      <label htmlFor="image1">
-                        <img src="image1.jpg" alt="Image 1" />
-                      </label>
-                    </td>
-                    <td>
-                      <input
-                        type="checkbox"
-                        id="image2"
-                        name="images[]"
-                        value="image2"
-                      />
-                      <label htmlFor="image2">
-                        <img src="image2.jpg" alt="Image 2" />
-                      </label>
-                    </td>
-                    <td>
-                      <input
-                        type="checkbox"
-                        id="image3"
-                        name="images[]"
-                        value="image3"
-                      />
-                      <label htmlFor="image3">
-                        <img src="image3.jpg" alt="Image 3" />
-                      </label>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
+            <p className="bold text-center">
+              Select categories that you might be interested in
+            </p>
+            <div className="text-center w-2/3 mx-auto grid grid-cols-[repeat(3,auto)] gap-1">
+              {categories.map((cat, index) => (
+                 <div className="bg-cover bg-center rounded-lg h-24 flex flex-col justify-center items-center" key={index} style={{ backgroundImage: `url(${cat.link})` }}>
+                 <span className="text-white font-semibold text-xs">{cat.name}</span>
+                 <input type="checkbox" className="mr-2" />
+               </div>
+              ))}
             </div>
-            <input
-              type="submit"
-              value="SIGN UP"
-              className="bg-FlamingoPink text-white"
-            />
           </form>
           <hr className="bg-gray" />
-          <div>
+          <div className=" text-center">
             <span>Already have an account?</span>{" "}
             <a href="/" className="text-gray">
               Log in
@@ -141,3 +146,29 @@ function Signup() {
 }
 
 export default Signup;
+
+{
+  /* <div className="   text-center w-2/3 mx-auto grid grid-cols-[repeat(3,auto)] gap-1">
+            {categories.map((cat, index)=>(
+              <div className="  " key={index} ><img src={cat.link} alt={cat.name} className="w-full h-full"/>
+              
+              </div>
+            ))}
+          </div> */
+}
+
+// <div
+//                   className="bg-cover bg-center rounded-lg h-24 relative"
+//                   key={index}
+//                   style={{ backgroundImage: `url(${cat.link})` }}
+//                 >
+//                   <div className="bg-black bg-opacity-50 w-full h-full absolute top-0 left-0">
+//                     <input
+//                       type="checkbox"
+//                       className="mr-2 absolute top-1/2 left-2"
+//                     />
+//                     <span className="text-white font-semibold absolute top-1/2 left-1/2">
+//                       {cat.name}
+//                     </span>
+//                   </div>
+//                 </div>
