b8949dbc21fd6de4d7c85a1506a0800e76c7e6f9
Rana <ranamohamad215@gmail.com>
2024-03-03 17:53:50 +0200

    Finished the logic of the signup page, except toggling the terms and conditions check

--- Aida/src/Authentication/Signup.jsx
+++ Aida/src/Authentication/Signup.jsx
@@ -1,21 +1,53 @@
 import { Link, useNavigate } from "react-router-dom";
 import LogoTealText from "../assets/logo/LogoTealText.svg";
 import categories from "../UI/SignupCategories";
+import { useState } from "react";
 const inputStyle = "border-solid border-2 border-gray px-2 py-1  mt-2 mb-4 ";
 const labelStyle = "font-semibold mx-2  ";
 const radioLabel = "text-gray hover:cursor-pointer";
 
-
 function Signup() {
+  const [checkedCategories, setCheckedCategories] = useState(
+    new Array(categories.length).fill(false)
+  );
+  const [formData, setFormData] = useState({
+    firstName: "",
+    secondName: "",
+    email: "",
+    password: "",
+    phone: "",
+    dateOfBirth: "",
+    gender: "",
+    city: "",
+    street: "",
+    buildingNumber: "",
+    apartmentNumber: "",
+    categories: [],
+    termsChecked: false,
+  });
 
   const navigate = useNavigate();
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setFormData((prevFormDate) => ({ ...prevFormDate, [name]: value }));
+  }
   function handleSubmit(e) {
     e.preventDefault();
-
-    navigate("/login");
+    const userData = {
+      ...formData,
+      categories: checkedCategories,
+    };
+    console.log(userData);
+    // navigate("/login");
   }
-  function handleChecked(e) {
-    console.log(e.targed.value);
+
+  // Sets an array of booleans for the categories
+  function handleChecked(position) {
+    const updatedCheckedCategories = checkedCategories.map((catState, index) =>
+      index === position ? !catState : catState
+    );
+    console.log(updatedCheckedCategories);
+    setCheckedCategories(updatedCheckedCategories);
   }
   return (
     <>
@@ -39,67 +71,38 @@ function Signup() {
                 <div>
                   <label className={labelStyle}>First Name</label> <br />
                   <input
+                    name="firstName"
                     type="text "
                     placeholder="Ahmad"
                     className={inputStyle}
+                    required
+                    maxLength="50"
+                    onChange={handleChange}
                   />
                 </div>
                 <div>
                   <label className={labelStyle}>Email</label>
                   <br />
                   <input
+                    name="email"
                     type="email"
                     placeholder="example@example.com"
                     className={inputStyle}
+                    maxLength="100"
+                    onChange={handleChange}
+                    required
                   />
                 </div>
-                <div>
-                  <label className={labelStyle}>Address</label>
-                  <br />
-                  <input
-                    type="text"
-                    placeholder="ElMoqatam, Cairo, Egypt"
-                    className={inputStyle}
-                  />
-                  <p className="font-thin text-xs ">
-                    This will be the delivery address
-                  </p>
-                </div>
+
                 <div className="mt-4">
                   <label className={labelStyle}>Date of birth</label>
                   <br />
-                  <input type="date" className={inputStyle} />
-                </div>
-                <div>
-                  <label className={labelStyle}>Account</label>
-                  <br />
-                  <div>
-                    <input
-                      type="radio"
-                      id="Shopping"
-                      value="Shopping"
-                      name="account"
-                      className={`${radioLabel} `}
-                      checked
-                    />
-                    <label className={`${radioLabel} `} htmlFor="Shopping">
-                      Shopping
-                    </label>
-                  </div>
-                  <div>
-                    <input
-                      type="radio"
-                      id="selling"
-                      value="Selling"
-                      name="account"
-                    />{" "}
-                    <label className={`${radioLabel} `} htmlFor="selling">
-                      Selling
-                    </label>
-                  </div>
-                  <p className="text-FlamingoPink">
-                    This cannot be changed later
-                  </p>
+                  <input
+                    type="date"
+                    className={inputStyle}
+                    name="dateOfBirth"
+                    onChange={handleChange}
+                  />
                 </div>
               </div>
               <div className="w-1/2 ">
@@ -107,53 +110,114 @@ function Signup() {
                   <label className={labelStyle}>Second Name</label>
                   <br />
                   <input
+                    name="secondName"
                     type="text"
                     placeholder="Taha"
                     className={inputStyle}
+                    maxLength="50"
+                    onChange={handleChange}
+                    required
                   />
                 </div>
                 <div>
                   <label className={labelStyle}>Password</label>
                   <br />
-                  <input type="password" className={inputStyle} />
+                  <input
+                    type="password"
+                    name="password"
+                    className={inputStyle}
+                    maxLength="50"
+                    onChange={handleChange}
+                    required
+                  />
                 </div>
                 <div>
                   <label className={labelStyle}>Phone</label>
                   <br />
                   <input
+                    name="phone"
                     type="tel"
                     placeholder="+201232453123"
                     className={inputStyle}
+                    onChange={handleChange}
+                    maxLength="15"
+                    required
                   />
                 </div>
-                <div className="mt-[50px] lg:mt-[38px]">
-                  <label className={labelStyle}>Gender</label>
-                  <br />
-                  <div>
-                    <input
-                      type="radio"
-                      id="male"
-                      value="Male"
-                      name="gender"
-                      checked
-                    />{" "}
-                    <label className={`${radioLabel} `} htmlFor="male">
-                      Male
-                    </label>
-                  </div>
-                  <div>
-                    <input
-                      type="radio"
-                      id="Female"
-                      name="gender"
-                      value="Female"
-                    />
-                    <label htmlFor="Female" className={`${radioLabel} `}>
-                      Female
-                    </label>
-                  </div>
+              </div>
+            </div>
+            <div className="mt-6 lg:mt-[38px]  mb-5 pl-16">
+              <label className={labelStyle}>Gender</label>
+              <br />
+              <div className="flex">
+                <div>
+                  <input
+                    type="radio"
+                    id="male"
+                    value="Male"
+                    name="gender"
+                    onChange={handleChange}
+                  />{" "}
+                  <label className={`${radioLabel} `} htmlFor="male">
+                    Male
+                  </label>
+                </div>
+                <div className="ml-4">
+                  <input
+                    type="radio"
+                    id="Female"
+                    name="gender"
+                    value="Female"
+                    onChange={handleChange}
+                  />
+                  <label htmlFor="Female" className={`${radioLabel}  `}>
+                    Female
+                  </label>
                 </div>
               </div>
+            </div>
+            <div className="">
+              <label className={`${labelStyle} pl-16`}>Billing Address</label>
+              <br />
+              <div className="flex">
+                <input
+                  type="text"
+                  placeholder="City"
+                  name="city"
+                  value={formData.city}
+                  className={`${inputStyle} w-32 mx-2  `}
+                  onChange={handleChange}
+                  required
+                />
+                <input
+                  type="text"
+                  placeholder="Street address"
+                  name="street"
+                  value={formData.street}
+                  className={`${inputStyle}  mx-2`}
+                  onChange={handleChange}
+                  required
+                />
+                <input
+                  type="text"
+                  placeholder="Building number"
+                  name="buildingNumber"
+                  value={formData.buildingNumber}
+                  className={`${inputStyle}w-32 mx-2 `}
+                  onChange={handleChange}
+                />
+                <input
+                  type="text"
+                  placeholder="Apartment number"
+                  name="apartmentNumber"
+                  value={formData.apartmentNumber}
+                  className={`${inputStyle}  mx-2 w-40`}
+                  onChange={handleChange}
+                />
+              </div>
+              <p className="font-thin text-xs ">
+                This will be the delivery address
+              </p>
             </div>
             <p className="font-bold mx-10 my-4 ">
               Select categories that you might be interested in
@@ -171,18 +235,42 @@ function Signup() {
                     </span>
                     <input
                       type="checkbox"
+                      value={cat.name}
                       className="mr-2 mb-3  "
-                      onChange={handleChecked}
+                      onChange={() => handleChecked(index)}
+                      checked={checkedCategories[index]}
                     />
                   </div>
                 </div>
               ))}
+            </div>
+            <div className="mt-4">
+              <input
+                type="checkbox"
+                value={formData.termsChecked}
+                name="termsChecked"
+                onChange={handleChange}
+                className="mr-2 mb-3  "
+              />
+              <p className="inline">
+                I accept{" "}
+                <span className="text-teal underline italic ">
+                  Terms of services
+                </span>{" "}
+                and{" "}
+                <span className="text-teal underline italic ">
+                  Privacy Policy
+                </span>
+              </p>
             </div>
             <div className="items-center flex justify-center w-full ">
               <input
                 className="text-white bg-FlamingoPink w-1/2  rounded-md h-8 uppercase my-5 cursor-pointer hover:translate-x-[2px] hover:translate-y-[2px] transition-all "
                 type="submit"
                 value="Signup"
+                disabled={!formData.termsChecked}
+                onChange={handleSubmit}
+                required
               />
             </div>
           </form>
