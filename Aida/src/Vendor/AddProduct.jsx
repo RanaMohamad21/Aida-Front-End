84ab5c55720ff21b259c096672fca87d6cceb22a
Rana <ranamohamad215@gmail.com>
2024-03-28 04:06:16 +0200

    Structure of Add Product page

--- Aida/src/Vendor/AddProduct.jsx
+++ Aida/src/Vendor/AddProduct.jsx
@@ -1,16 +1,48 @@
 import { useShelves } from "../Contexts/VendorShelves";
+import VendorNavBar from "./VendorNavBar";
 function AddProduct(props) {
   // eslint-disable-next-line react/prop-types
   console.log(props);
   // eslint-disable-next-line react/prop-types
   //   const { shelfID } = state;
   const { updateShelves, shelfID } = useShelves();
+  function handleAddProduct(){
+
+  }
+
   console.log("Shelf ID: ", shelfID);
   return (
-    <div>
-      <h1>Shelf ID: {shelfID}</h1>
-      {/* You can render other components or UI elements related to adding a product */}
+    <>
+    <VendorNavBar />
+    <div className=" grid-rows-[auto,1fr]">
+      <form>
+      {/* Upload Image  */}
+      <div></div>
+
+
+      {/* Item Information */}
+      <div className=" grid-rows-5">
+        {/* Title  */}
+        <div ><p className=" mx-auto text-teal ">product info</p>
+        </div>
+
+        {/* Info and Pricing    */}
+        <div></div>
+
+        {/* Specification  */}
+        <div></div>
+
+        {/* Tags  */}
+        <div></div>
+
+        {/* Description  */}
+        <div></div>
+
+      </div>  
+ 
+  </form>
     </div>
+    </>
   );
 }
 
