f4b25cf0e9f8376d6d4e692861920fc667109307
Rana <ranamohamad215@gmail.com>
2024-04-17 15:03:50 +0200

    Added login validtion

--- Aida/src/Authentication/AuthenticationContext.jsx
+++ Aida/src/Authentication/AuthenticationContext.jsx
@@ -1,13 +1,16 @@
 import { useContext, useReducer, useState } from "react";
 import { createContext } from "react";
 
+// Create a new context for authentication state
 const AuthenticationContext = createContext();
 
+// Define the initial state for authentication
 const initialState = {
   user: null,
   isAuthenticated: false,
 };
 
+// A fake user for testing purposes
 const FAKE_USER = {
   name: "Rana",
   email: "Rana@trial.com",
@@ -15,35 +18,80 @@ const FAKE_USER = {
   avatar: "link",
 };
 
+// Reducer function to handle state changes
 function reducer(state, action) {
   switch (action.type) {
     case "login":
-      // state is kept here to set acout type [customuer/vendor]
+      // Update the state with the user type  and set isAuthenticated to true
       return { ...state, user: action.payload, isAuthenticated: true };
     case "logout":
+      // Reset the state to the initial state
       return { ...state, user: null, isAuthenticated: false };
     default:
       throw new Error("Unknown action");
   }
 }
+// AuthenticationProvider component
 // eslint-disable-next-line react/prop-types
 function AuthenticationProvider({ children }) {
+  // Use the reducer to manage state
   const [{ user, isAuthenticated }, dispatch] = useReducer(
     reducer,
     initialState
   );
+
+  // Use state to manage error messages
   const [error, setError] = useState("");
-  function login(email, password) {
-    if (email === FAKE_USER.email && password === FAKE_USER.password)
+
+  //TODO: Check the user type [Customer/Vendor]
+  // Login function to authenticate a user
+  // Validate email and password
+  function isValidEmail(email) {
+    const re =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
+
+  function isValidPassword(password) {
+    return password.length >= 8;
+  }
+
+  // Login function with email and password validation
+  async function login(email, password) {
+    if (!isValidEmail(email)) {
+      setError("Please enter a valid email address");
+      return;
+    }
+
+    if (!isValidPassword(password)) {
+      setError(
+        "Please enter a valid password. It should be at least 8 characters long"
+      );
+      return;
+    }
+
+    setError("");
+
+    try {
+      // Authenticate user
+      // const response = await api.login(email, password);
+      // dispatch({ type: "login", payload: response.data });
+
+      // For testing purposes, we will just log in with the fake user
       dispatch({ type: "login", payload: FAKE_USER });
-    else {
-        setError("Incorrect credentials!")
+    } catch (error) {
+      console.error("Failed to login:", error);
+      setError("Failed to log in. Please check your email and password");
     }
   }
+  // Logout function to log out the user
   function logout() {
     dispatch({ type: "logout" });
   }
   return (
+    /* Provide the user, isAuthenticated, login & logout functions 
+    and error properties to the context */
+
     <AuthenticationContext.Provider
       value={{ user, isAuthenticated, login, logout, error }}
     >
@@ -52,13 +100,20 @@ function AuthenticationProvider({ children }) {
   );
 }
 
+// Custom hook to use the AuthenticationContext
 function useAuthentication() {
+  // Get the context
   const context = useContext(AuthenticationContext);
+
+  // If the context is not available, throw an error
   if (context === undefined)
     throw new Error(
       "AuthenticationContext was used outside AuthenticationProvider"
     );
+
+  // Else return the context
   return context;
 }
 
+// Export the custom hook and the AuthenticationProvider component
 export { useAuthentication, AuthenticationProvider };
--- Aida/src/Authentication/Login.jsx
+++ Aida/src/Authentication/Login.jsx
@@ -1,28 +1,40 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 
+import { useAuthentication } from "./AuthenticationContext";
 import logoWhiteTxtAida from "/src/assets/logo/LogoWhiteTextAida.svg";
 import LoginImg from "../assets/Authentication/login.png";
-import { useAuthentication } from "./AuthenticationContext";
 
 function Login() {
+  // Use the useNavigate hook to navigate to a different page
   const navigate = useNavigate();
+
+ // Set up state for email and password
   const [email, setEmail] = useState("example@example.com");
   const [password, setPassword] = useState("");
 
+   // Get the login function, isAuthenticated, and error properties from the useAuthentication hook
   const { login, isAuthenticated, error } = useAuthentication();
 
+  // Reset the input fields
   function resetInputs() {
     setEmail(" ");
     setPassword(" ");
   }
+
+  // Handle form submission
   function handleSubmit(e) {
     e.preventDefault();
-    if (email && password) login(email, password);
+    if (email && password) {
+      login(email, password);
+    }
     resetInputs();
   }
+
+  // Use the useEffect hook to navigate to the home page if the user is authenticated
   useEffect(
     function () {
+      // User can't navigate back to the login page after login is completed
       if (isAuthenticated) navigate("/", { replace: true });
       else {
         console.log(error);
@@ -32,6 +44,7 @@ function Login() {
   );
   return (
     <div className=" h-screen w-full flex items-start pb-10 ">
+      {/* Display the login image */}
       <div className="relative w-3/5 h-full flex flex-col ">
         <img
           src={LoginImg}
@@ -39,6 +52,7 @@ function Login() {
           className="w-full h-full object-cover "
         />
       </div>
+      {/* Display the login form */}
       <div className="font-sans w-2/5 h-full bg-teal text-white pb-10  flex flex-col justify-between border-solid border-l-[1px] border-l-gray">
         <img src={logoWhiteTxtAida} alt="Logo" className="h-1/2 " />
         <form
@@ -47,6 +61,7 @@ function Login() {
           action=""
           onSubmit={handleSubmit}
         >
+          {/* Display the email input field */}
           <div className="py-4">
             <label className="text-xl">Email</label>
             <input
@@ -57,6 +72,7 @@ function Login() {
               required
             />
           </div>
+           {/* Display the password input field */}
           <div className="py-4">
             <label className="text-xl">Password</label>
             <input
@@ -67,9 +83,14 @@ function Login() {
               required
             />
           </div>
+
+          {/* Display a link to reset the password */}
+          {/*/TODO: Make it a link*/}
           <p className="font-thin text-IceBlue text-xs">
             forgot your password?
           </p>
+
+          {/* Display the login button */}
           <button className="bg-FlamingoPink w-full rounded-md h-8 my-5 cursor-pointer hover:translate-x-[2px] hover:translate-y-[2px] transition-all uppercase">
             Login
           </button>
