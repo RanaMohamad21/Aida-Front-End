7e4ecd369b64e6bc418dc8d66313dff939db5f6d
Rana <ranamohamad215@gmail.com>
2024-05-03 11:16:33 +0300

    Fnished the lay out and the form of the search page and added pagination component

---
+++ Aida/src/Store/PaginatedProducts.jsx
@@ -0,0 +1,49 @@
+
+// Reusable component for data pagination
+function PaginatedProducts() {
+    return (
+        // Dummy Content
+        <div className=" p-6  ">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac odio
+          vitae ante molestie suscipit. Pellentesque vel fringilla ipsum, sed
+          maximus arcu. Nulla vehicula arcu at nisl varius, ut pretium erat
+          ultricies. Praesent vel mauris in ante semper finibus vel vitae enim.
+          Phasellus at nulla erat. In tincidunt elementum lacus at ornare. Morbi
+          felis sem, tempus at vestibulum id, tempor eu erat. Orci varius
+          natoque penatibus et magnis dis parturient montes, nascetur ridiculus
+          mus. Donec tempor metus sit amet vulputate suscipit. Suspendisse
+          suscipit ullamcorper lectus, sit amet tincidunt nisi mollis vitae.
+          Cras lacus augue, viverra a tincidunt a, fringilla a quam. Praesent
+          non sodales nunc. Nullam eu metus eu sapien efficitur cursus. Fusce
+          placerat tortor id suscipit maximus. Nam at blandit nisi. Phasellus ut
+          sollicitudin odio. Etiam rhoncus, leo id suscipit facilisis, lectus
+          neque euismod orci, in molestie tellus ante vel odio. Mauris id
+          pharetra metus. Sed sodales facilisis tortor eu tempus. Maecenas at
+          arcu ultrices, interdum lectus sit amet, hendrerit quam. Nullam
+          iaculis convallis diam eu semper. Integer ipsum quam, placerat sed est
+          in, dignissim ultrices ligula. Sed luctus risus mauris, sit amet
+          iaculis augue porttitor in. Aenean et pellentesque odio. In a maximus
+          odio. Nullam viverra risus risus. Integer erat dolor, accumsan vel
+          mauris et, pellentesque suscipit erat. Sed rhoncus ut purus sit amet
+          aliquam. Vivamus faucibus mauris a ligula viverra porta. Etiam lacinia
+          ligula efficitur dui consectetur tempus. Vivamus lobortis gravida eros
+          et tempus. Pellentesque at massa massa. In egestas eros libero, eget
+          convallis ante euismod vel. Mauris id ultricies odio, vitae
+          pellentesque ex. Sed et leo nisl. Phasellus fermentum, orci eu viverra
+          efficitur, nulla odio vehicula nisi, sit amet efficitur mauris nibh
+          pharetra nunc. In ipsum urna, sagittis quis odio in, bibendum luctus
+          dolor. Nullam ac lacus ultrices, facilisis ex vitae, viverra erat.
+          Pellentesque et volutpat magna, ac elementum ipsum. Nam dignissim at
+          sapien ac porta. Praesent pulvinar neque sapien, in vehicula nulla
+          tempor non. Phasellus rhoncus pellentesque magna, sed fringilla leo
+          rutrum id. Sed gravida lorem non sodales dapibus. In hac habitasse
+          platea dictumst. Maecenas placerat dolor sit amet felis vestibulum, a
+          gravida metus finibus. Etiam luctus lectus nec scelerisque rutrum.
+          Interdum et malesuada fames ac ante ipsum primis in faucibus.
+          Phasellus ultricies tempor mi, in imperdiet ipsum congue sit amet.
+          Quisque consectetur vitae felis quis molestie.
+        </div>
+    )
+}
+
+export default PaginatedProducts;
--- Aida/src/Store/SearchPage.jsx
+++ Aida/src/Store/SearchPage.jsx
@@ -4,10 +4,14 @@ import Searchbar from "../UI/Searchbar";
 import StarRating from "../UI/StarRating";
 import { DevTool } from "@hookform/devtools";
 import { useForm } from "react-hook-form";
+import UpperFooter from "../UI/UpperFooter";
+import PaginatedProducts from "./PaginatedProducts";
 
 function SearchPage() {
   const [starRating, setStarRating] = useState(0);
-  const [errors, setErrors] = useState("");
+  const [errors, setErrors] = useState(""); // State for form errors
+ 
+  // React Hook Form setup
   const {
     register,
     handleSubmit,
@@ -17,6 +21,8 @@ function SearchPage() {
   function onSubmit(data) {
     console.log(data);
     console.log(starRating);
+    
+      // Check if minimum price is greater than or equal to maximum price
     if(data.minPrice >= data.maxPrice){
       setErrors("The minimum price must be smaller than the maximum price.")
       return;
@@ -31,11 +37,14 @@ function SearchPage() {
       <Navbar />
       <div className="grid grid-cols-[1fr,4fr]  ">
         {/* Search Filters */}
+        <div className=" relative ">
+          <div className=" sticky top-10 left-0 right-0 z-[1]"> 
+          
         <form
           className=" border-r-IceBlue border-r-solid border-r-[3px]  "
           onSubmit={handleSubmit(onSubmit)}
         >
-          <div className=" text-teal py-3 pl-5 fixed  rounded-lg  ">
+          <div className=" text-teal py-3 pl-5  rounded-lg  ">
             <h1 className=" text-[23px] text-FlamingoPink">Search filters</h1>
             {/* Rating component */}
             <p className=" text-lg mt-3 mb-1">Minimum rating</p>
@@ -48,7 +57,6 @@ function SearchPage() {
             <input
               type="text"
               placeholder="00.00"
-              // defaultValue={0} // Set default value for minPrice as 0
               className={`border border-gray border-solid pl-1 mb-4 w-[50%]`}
               {...register("minPrice", {
                 setValueAs: (value) => parseFloat(value)
@@ -58,15 +66,13 @@ function SearchPage() {
             <label className=" block text-black text-sm mt-2">Max</label>
             <input
               type="text"
-              // Set default value for maxPrice as maximum float value
-              placeholder="00.00"
-              // value={Number.MAX_VALUE}
-              className={`border border-gray border-solid pl-1 mb-4 w-[50%] bg-white`}
+               placeholder="00.00"
+               className={`border border-gray border-solid pl-1 mb-4 w-[50%] bg-white`}
               {...register("maxPrice", {
                 setValueAs: (value) => parseFloat(value),
               })}
             />
-            {/* Display error message if validation fails */}
+            {/* Display error message if  price range validation fails */}
             {errors && (
               <p className=" text-FlamingoPink w-[50%] pb-4"> {errors} </p>
             )}
@@ -118,49 +124,16 @@ function SearchPage() {
             />
           </div>
         </form>
-        <DevTool control={control} />
-        {/* Products */}
-        <div className="">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac odio
-          vitae ante molestie suscipit. Pellentesque vel fringilla ipsum, sed
-          maximus arcu. Nulla vehicula arcu at nisl varius, ut pretium erat
-          ultricies. Praesent vel mauris in ante semper finibus vel vitae enim.
-          Phasellus at nulla erat. In tincidunt elementum lacus at ornare. Morbi
-          felis sem, tempus at vestibulum id, tempor eu erat. Orci varius
-          natoque penatibus et magnis dis parturient montes, nascetur ridiculus
-          mus. Donec tempor metus sit amet vulputate suscipit. Suspendisse
-          suscipit ullamcorper lectus, sit amet tincidunt nisi mollis vitae.
-          Cras lacus augue, viverra a tincidunt a, fringilla a quam. Praesent
-          non sodales nunc. Nullam eu metus eu sapien efficitur cursus. Fusce
-          placerat tortor id suscipit maximus. Nam at blandit nisi. Phasellus ut
-          sollicitudin odio. Etiam rhoncus, leo id suscipit facilisis, lectus
-          neque euismod orci, in molestie tellus ante vel odio. Mauris id
-          pharetra metus. Sed sodales facilisis tortor eu tempus. Maecenas at
-          arcu ultrices, interdum lectus sit amet, hendrerit quam. Nullam
-          iaculis convallis diam eu semper. Integer ipsum quam, placerat sed est
-          in, dignissim ultrices ligula. Sed luctus risus mauris, sit amet
-          iaculis augue porttitor in. Aenean et pellentesque odio. In a maximus
-          odio. Nullam viverra risus risus. Integer erat dolor, accumsan vel
-          mauris et, pellentesque suscipit erat. Sed rhoncus ut purus sit amet
-          aliquam. Vivamus faucibus mauris a ligula viverra porta. Etiam lacinia
-          ligula efficitur dui consectetur tempus. Vivamus lobortis gravida eros
-          et tempus. Pellentesque at massa massa. In egestas eros libero, eget
-          convallis ante euismod vel. Mauris id ultricies odio, vitae
-          pellentesque ex. Sed et leo nisl. Phasellus fermentum, orci eu viverra
-          efficitur, nulla odio vehicula nisi, sit amet efficitur mauris nibh
-          pharetra nunc. In ipsum urna, sagittis quis odio in, bibendum luctus
-          dolor. Nullam ac lacus ultrices, facilisis ex vitae, viverra erat.
-          Pellentesque et volutpat magna, ac elementum ipsum. Nam dignissim at
-          sapien ac porta. Praesent pulvinar neque sapien, in vehicula nulla
-          tempor non. Phasellus rhoncus pellentesque magna, sed fringilla leo
-          rutrum id. Sed gravida lorem non sodales dapibus. In hac habitasse
-          platea dictumst. Maecenas placerat dolor sit amet felis vestibulum, a
-          gravida metus finibus. Etiam luctus lectus nec scelerisque rutrum.
-          Interdum et malesuada fames ac ante ipsum primis in faucibus.
-          Phasellus ultricies tempor mi, in imperdiet ipsum congue sit amet.
-          Quisque consectetur vitae felis quis molestie.
         </div>
+        </div>
+        {/* For testing purposes */}
+        <DevTool control={control} />
+        
+        <PaginatedProducts/>
+        
+        
       </div>
+      <UpperFooter/>
     </>
   );
 }
