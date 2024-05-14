788c4b92589cc2e6dea57a906e9bd04cea9acdff
Rana <ranamohamad215@gmail.com>
2024-05-14 22:37:28 +0300

    Added a dummy page to test individual components.

---
+++ Aida/src/UI/dummyPage.jsx
@@ -0,0 +1,29 @@
+import "react-multi-carousel/lib/styles.css";
+
+
+  // This is a dummy page, 
+  //to test individual components
+function DummyPage() {
+    return (
+        
+            <>
+            <div className=" text-FlamingoPink w-full">
+                <p>This is a dummy page for testing components</p>
+
+            
+        </div>
+
+  <div>
+    {/* put the component you want to test here */}
+  </div>
+            </>
+     
+    
+
+
+    )
+}
+
+
+
+export default DummyPage;
