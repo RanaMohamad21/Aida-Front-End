dcb44108a7984463df8b205f9907ad5690f6865e
Rana <ranamohamad215@gmail.com>
2024-05-14 22:40:20 +0300

    Created page.

---
+++ Aida/src/Vendor/VendorProductViewPage.jsx
@@ -0,0 +1,13 @@
+import ProductViewPage from "../Store/ProductViewPage";
+
+
+function VendorProductViewPage() {
+    return (
+        <>
+            <ProductViewPage isVendor={true}/>
+        </>
+    )
+}
+
+
+export default VendorProductViewPage;
