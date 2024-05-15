3149016c7246c5ad31a764dc452eef2b931e481b
Rana <ranamohamad215@gmail.com>
2024-05-16 01:54:49 +0300

    Edits.

--- Aida/src/UI/Shelf.jsx
+++ Aida/src/UI/Shelf.jsx
@@ -4,11 +4,15 @@ import PropTypes from "prop-types";
 
 // This reusable component consistes of a shelf name and the
 // children represents the shelf content
-function Shelf({ shelfName, styles, children }) {
+function Shelf({ shelfName, styles, children, icon = "" }) {
   return (
     <div className={`flex flex-col justify-between md:mr-4`}>
       <div className="p-2 bg-teal inline-block max-w-max text-white ml-4 ">
-        {shelfName}
+        {icon == ""?<span>{shelfName}</span>:
+        <>
+        <span className=" inline-block pr-2 ">{icon}</span><span>{shelfName}</span>
+        </>
+        }
       </div>
       <div className={`${styles} `}>
         
@@ -18,6 +22,13 @@ function Shelf({ shelfName, styles, children }) {
   );
 }
 
-Shelf.propTypes = {};
+
+
+Shelf.propTypes = {
+  styles: PropTypes.string,
+  shelfName: PropTypes.string.isRequired,
+  children: PropTypes.any,
+  icon: PropTypes.string
+};
 
 export default Shelf;
