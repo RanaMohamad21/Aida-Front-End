63bd5dab12c2b072d1e76516b1996bd1e32fe7fd
Rana <ranamohamad215@gmail.com>
2024-05-18 01:15:54 +0300

    Edited dimensions.

--- Aida/src/UI/Navbar.jsx
+++ Aida/src/UI/Navbar.jsx
@@ -15,10 +15,42 @@ const NavbarLinks = [
 
 function Navbar() {
   return (
-    <nav className="w-[100%] bg-teal mt-1 h-10  ">
+    <nav className="md:w-screen bg-teal mt-1  h-10 text-md  ">
       <div className="   text-white font-sans  w-full h-full  ">
         {/*Menu items*/}
-        <ul className="flex justify-between text-nowrap items-center gap-12 h-full pr-3">
+        <ul className=" grid grid-cols-[repeat(10,auto)] gap-2 text-nowrap   h-full ">
+          <li className="bg-FlamingoPink  h-full px-2 ">
+            <NavLink
+              to="/signupvendor1"
+              className="inline-block hover:text-IceBlue  duration-200"
+            >
+              <div className=" flex items-center justify-center gap-2 pt-2 "> 
+              <span >
+              <CreateShopIcon/>
+              </span>
+              Create your shop today!
+              </div>
+            </NavLink>
+          </li>
+          {NavbarLinks.map((link, index) => (
+            <li key={index} className=" h-full">
+              <NavLink
+                to={link.link}
+                className="inline-block pt-2 h-full hover:text-IceBlue hover:border   px-1 hover:bg-[#03bbbf] duration-200"
+              >
+                <span>{link.name}</span>
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
+
+{/* <ul className="flex justify-between text-nowrap items-center gap-12 h-full pr-3">
           <li className="bg-FlamingoPink h-full px-2 ">
             <NavLink
               to="/signupvendor1"
@@ -42,10 +74,4 @@ function Navbar() {
               </NavLink>
             </li>
           ))}
-        </ul>
-      </div>
-    </nav>
-  );
-}
-
-export default Navbar;
+        </ul> */}
