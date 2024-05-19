5c1e1455cd3622566443adcf277c561ee8230dd3
Rana <ranamohamad215@gmail.com>
2024-05-20 00:00:46 +0300

    Added shopping cart context.

---
+++ Aida/src/Contexts/ShoppingCartProvider.jsx
@@ -0,0 +1,99 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import PropTypes from "prop-types";
+
+const ShoppingCartContext = createContext({});
+
+function ShoppingCartProvider({ children }) {
+  // Array of objects, each containing a product ID and its quantity
+  const [cartItems, setCartItems] = useState([]);
+
+  //?  For testing purposes:
+  useEffect(() => {
+    console.log(
+      "⚠️⚠️ Displaying the cart content in the cart context for testing purposes. ⚠️⚠️"
+    );
+    console.log("Cart Items: ", cartItems);
+  }, [cartItems]);
+
+  // Returns the quantity selected by the customer for the given product ID
+  function getItemsQuantity(id) {
+    return cartItems.find((item) => item.id === id)?.quantity || 0;
+  }
+
+  // Function to add a new item to the cart or increase the quantity of an existing one
+  function increaseItemQuantity(id) {
+    setCartItems((currentItems) => {
+      // Checks if the item is not already in the cart it adds it
+      if (currentItems.find((item) => item.id === id) == null) {
+        return [...currentItems, { id, quantity: 1 }];
+      }
+      //  If the item already exists in the cart, increase its quantity.
+      else {
+        return currentItems.map((item) => {
+          if (item.id === id) {
+            return { ...item, quantity: item.quantity + 1 };
+          } else {
+            return item;
+          }
+        });
+      }
+    });
+  }
+
+  // A Function to decrease the quanitity of an existing item in the cart.
+  function decreaseItemQuantity(id) {
+    setCartItems((currentItems) => {
+      const itemIndex = currentItems.findIndex((item) => item.id === id);
+
+      // If the item is not found in the cart, return current items
+      if (itemIndex === -1) {
+        return currentItems;
+      }
+
+      // If the item exists in the cart
+      const updatedItems = [...currentItems];
+      const currentItem = updatedItems[itemIndex];
+
+      // If the quantity is more than 1, decrease it
+      if (currentItem.quantity > 1) {
+        currentItem.quantity -= 1;
+      } else {
+        // If the quantity is 1, remove the item from the cart
+        updatedItems.splice(itemIndex, 1);
+      }
+
+      return updatedItems;
+    });
+  }
+
+  function removeItemFromCart(id) {
+    setCartItems((currentItems) =>
+      currentItems.filter((item) => item.id !== id)
+    );
+  }
+
+  return (
+    <ShoppingCartContext.Provider
+      value={{
+        cartItems,
+        increaseItemQuantity,
+        decreaseItemQuantity,
+        removeItemFromCart,
+        getItemsQuantity,
+      }}
+    >
+      {children}
+    </ShoppingCartContext.Provider>
+  );
+}
+
+ShoppingCartProvider.propTypes = {
+  children: PropTypes.any,
+};
+
+export default ShoppingCartProvider;
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const useShoppingCart = () => {
+  return useContext(ShoppingCartContext);
+};
