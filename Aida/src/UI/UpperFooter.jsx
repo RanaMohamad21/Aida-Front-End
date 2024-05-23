8851c89de9cc8b0457ed2d1b89ea5f2a981ea11e
Rana <ranamohamad215@gmail.com>
2024-05-23 18:37:02 +0300

    Uppdated icons.

--- Aida/src/UI/UpperFooter.jsx
+++ Aida/src/UI/UpperFooter.jsx
@@ -1,25 +1,45 @@
+import { Link } from "react-router-dom";
 import logoWhiteTxtAida from "/src/assets/logo/LogoWhiteTextAida.svg";
-import babyCarriage from "/src/assets/home/baby-carriage.png";
+
+
+import {
+  Activity,
+  Android,
+  BookFill,
+  BuildingsFill,
+  HandbagFill,
+  HouseFill,
+  Mouse2Fill,
+} from "react-bootstrap-icons";
 const iconList = [
-  [
-    { name: "babyCarriage", alt: "babyCarriage" },
-    { name: "babyCarriage", alt: "babyCarriage" },
-    { name: "babyCarriage", alt: "babyCarriage" },
-  ],
-  [
-    { name: "babyCarriage", alt: "babyCarriage" },
-    { name: "babyCarriage", alt: "babyCarriage" },
-    { name: "babyCarriage", alt: "babyCarriage" },
-  ],
-  [
-    { name: "babyCarriage", alt: "babyCarriage" },
-    { name: "babyCarriage", alt: "babyCarriage" },
-    { name: "babyCarriage", alt: "babyCarriage" },
-  ],
+  
+    { 
+      name: HandbagFill , alt: "fashion" },
+    { name: Mouse2Fill, alt: "electronics" },
+    { name: Activity, alt: "health" },
+    { name: Android, alt: "pet" },
+    { name: BookFill, alt: "books" },
+    { name: BuildingsFill, alt: "office supplies" },
+    { name: HouseFill, alt: "Home essencials" },
+   
 ];
 function UpperFooter() {
+  const scrollToTop = () => {
+    const scrollDuration = 1000; // Duration of the scroll in milliseconds
+    const scrollStep = -window.scrollY / (scrollDuration / 15);
+
+    const scroll = () => {
+      if (window.scrollY !== 0) {
+        window.scrollBy(0, scrollStep);
+        requestAnimationFrame(scroll);
+      }
+    };
+
+    requestAnimationFrame(scroll);
+  };
+
   return (
-    <div className="bg-teal  flex justify-between items-center px-4 text-white  w-full mb-12  bottom-[49px] ">
+    <div className="bg-teal  flex justify-between items-center pl-4 text-white  w-full pb-20 ">
       <div className="justify-self-start ">
         <img src={logoWhiteTxtAida} alt="logo" />
         <p className="font-semibold ">Developers</p>
@@ -31,51 +51,30 @@ function UpperFooter() {
           <li>Hanan Saeed Mohamad</li>
         </ul>
       </div>
-      <div className="justify-self-end">
-        <div className="border-2 p-3px w-[20px]  transition-all duration-200 hover:border-IceBlue ">
-          <button>&uarr;</button>
+      <div className="flex flex-col justify-around  ">
+        <div className=" w-full flex pr-6 justify-end">
+        <div className="border-2 w-[20px]  transition-all duration-200 hover:border-IceBlue flex items-center justify-center pb-1">
+          <button onClick={scrollToTop}> &uarr;</button>
         </div>
-        <p>Categories</p>
+        </div>
+        <p className=" my-3">Categories</p>
         <div>
-          <div className="flex gap-2">
-            <div className="rounded-full bg-white p-[4px]">
-              <img src={babyCarriage} />
-            </div>
-
-            <div className="rounded-full bg-white p-[4px]">
-              <img src={babyCarriage} />
-            </div>
-
-            <div className="rounded-full bg-white p-[4px]">
-              <img src={babyCarriage} />
-            </div>
+          <div className="grid grid-cols-3 gap-2 w-52">
+          {iconList.map((icon, index) => {
+        const IconComponent = icon.name;
+        return (
+          <div key={index} title={icon.alt} className=" w-10 flex items-center justify-center bg-white rounded-full text-teal">
+            <Link>
+            <IconComponent aria-label={icon.alt} className="p-2 w-full h-full" />
+            </Link>
           </div>
-          <div className="flex gap-2 my-[8px]">
-            <div className="rounded-full bg-white p-[4px]">
-              <img src={babyCarriage} />
-            </div>
+        );
+      })}
 
-            <div className="rounded-full bg-white p-[4px]">
-              <img src={babyCarriage} />
-            </div>
-
-            <div className="rounded-full bg-white p-[4px]">
-              <img src={babyCarriage} />
-            </div>
+          
           </div>
-          <div className="flex gap-2">
-            <div className="rounded-full bg-white p-[4px]">
-              <img src={babyCarriage} />
-            </div>
-
-            <div className="rounded-full bg-white p-[4px]">
-              <img src={babyCarriage} />
-            </div>
-
-            <div className="rounded-full bg-white p-[4px]">
-              <img src={babyCarriage} />
-            </div>
-          </div>
+         
+          
         </div>
       </div>
     </div>
