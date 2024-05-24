472ab28f1e88e0ebc1dcae5d23dd964e6f6d8193
Rana <ranamohamad215@gmail.com>
2024-05-24 18:04:39 +0300

    Trial.

--- Aida/src/Store/HomePage.jsx
+++ Aida/src/Store/HomePage.jsx
@@ -149,6 +149,9 @@ function HomePage() {
           <h1 className=" text-2xl text-FlamingoPink font-semibold">
             Most of the links are here for now
           </h1>
+          <h1 className=" text-2xl text-FlamingoPink font-semibold">
+            Most of the links are here for now
+          </h1>
           <Link to="/productviewpage">Product View Page</Link>
           <Link to="/VendorProductViewPage">Vendor Product View Page</Link>
           <Link to="/shoppingCart">Shopping Cart Page</Link>
