41300bb144050da9acff46699ad8b0ac951c076e
Rana <ranamohamad215@gmail.com>
2024-05-26 16:25:03 +0300

    Added nested routes.

--- Aida/src/Customer/CustomerProfilePage.jsx
+++ Aida/src/Customer/CustomerProfilePage.jsx
@@ -1,17 +1,15 @@
+import { Outlet } from "react-router-dom";
 import Searchbar from "../UI/Searchbar";
 import CustomerNavBar from "./CustomerNavBar";
-// import CustomerOrdersHistory from "./CustomerOrdersHistory";
-// import OrderInfo from "./OrderInfo";
-// import PendingOrders from "./PendingOrders";
-import SettingsPage from "./SettingsPage";
+
 
 function CustomerProfilePage() {
   return (
     <div className="grid grid-rows-[auto,1fr]">
       <Searchbar  isProfilePage={true}/>
-      <div className=" grid sm:grid-cols-[30%,3fr] lg:grid-cols-[20%,3fr] gap-3 h-screen ">
+      <div className=" grid sm:grid-cols-[auto,3fr] lg:grid-cols-[20%,3fr] gap-3 h-screen ">
         <CustomerNavBar />
-        <SettingsPage/>
+        <Outlet/>
       </div>
     </div>
   );
